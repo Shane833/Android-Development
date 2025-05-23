377412b7b33f44a7ca83c9de389fcbebe2e50476
Shane833 <williamsshane883@gmail.com>
2025-05-23 17:08:57 +0530

    A little switching

--- Head_First_Andriod_Developement/Chapter_2-Building_Interactive_Apps/2-Beer_Advisor/app/src/main/java/com/example/beeradvisor/FindBeerActivity.java
+++ Head_First_Andriod_Developement/Chapter_2-Building_Interactive_Apps/2-Beer_Advisor/app/src/main/java/com/example/beeradvisor/FindBeerActivity.java
@@ -15,7 +15,7 @@ public class FindBeerActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_find_beer);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
